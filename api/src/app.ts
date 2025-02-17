982464ad6698934411e7174885f31b860e3d2bb4
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2025-02-17 18:29:14 -0300

    Initialize Express application and set up static file serving

---
+++ api/src/app.ts
@@ -0,0 +1,11 @@
+import express, { Application } from "express";
+import { Routes } from "./routes/routes";
+import path from "path";
+
+const app: Application = express();
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.use("/api", Routes);
+
+export default app;
