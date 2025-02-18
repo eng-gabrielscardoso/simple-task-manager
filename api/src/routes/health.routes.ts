1e0900d841c76c45329903a54c1cedcd1d661016
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2025-02-18 14:33:18 -0300

    Refactor approach in health routes

--- api/src/routes/health.routes.ts
+++ api/src/routes/health.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import HealthController from "../controllers/health.controller";
+import { HealthController } from "../controllers/health.controller";
 
 const router: Router = Router();
 
