682cbe1d50fd99212f259dcb754467e9409d8ae0
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2025-02-18 17:07:36 -0300

    Change theme to better fit with brand colours

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { PrimeReactProvider } from 'primereact/api';
 import "./index.css";
-import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primereact/resources/themes/lara-dark-amber/theme.css";
 import { App } from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
