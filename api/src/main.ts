77d419dd53a9601a13f930de7fd77b56d85097bf
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2025-02-18 10:49:43 -0300

    Refactor main.ts to initialize database connection and use secrets for API port

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,14 +1,19 @@
-import * as dotenv from "dotenv-safe";
-import app from "./app";
+import { App } from "./app";
+import { ApiDataSource } from "./data-source";
+import secrets from "./helpers/secrets";
 
-dotenv.config();
+ApiDataSource.initialize()
+  .then(() => {
+    console.log("Database connection successfully");
 
-app
-  .listen(process.env.API_PORT, () =>
-    console.log(`Server running on port: ${process.env.API_PORT}`)
-  )
-  .on("error", (err) =>
-    console.error(
-      `An error occurred during application bootstrapping. Error: ${err}`
-    )
+    App.listen(secrets.API_PORT, () =>
+      console.log(`Server running on port: ${secrets.API_PORT}`)
+    ).on("error", (err) =>
+      console.error(
+        `An error occurred during application bootstrapping. Error: ${err}`
+      )
+    );
+  })
+  .catch((err) =>
+    console.error(`Error during database connection. Details: ${err}`)
   );
