758392fe85f26461f7e5f6f1462f3cf4849d67f6
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2025-02-18 14:34:20 -0300

    Assign routes for task in main router

--- api/src/routes/routes.ts
+++ api/src/routes/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { HealthRouter } from "./health.routes";
+import { TaskRouter } from "./task.routes";
 
 const router: Router = Router();
 
 router.use(HealthRouter)
+router.use(TaskRouter)
 
 export const Routes: Router = router;
