f0ffbbeefb44d1344c028b4849e85b8916be7ccb
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2025-02-18 14:34:08 -0300

    Create task routes

---
+++ api/src/routes/task.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { TaskController } from "../controllers/task.controller";
+
+const router: Router = Router();
+
+router.get("/tasks", TaskController.index);
+router.post("/tasks", TaskController.store);
+router.patch("/tasks/:id", TaskController.update);
+router.delete("/tasks/:id", TaskController.destroy);
+
+export const TaskRouter: Router = router;
