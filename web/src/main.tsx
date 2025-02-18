5dbf8ae9362fc7b84b7a909ee778a0728919295e
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2025-02-18 17:17:10 -0300

    Install and register prime flex for better CSS utilities

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { PrimeReactProvider } from 'primereact/api';
-import "./index.css";
-import "primereact/resources/themes/lara-dark-amber/theme.css";
 import { App } from './App.tsx'
+
+import "./index.css";
+import "primeflex/primeflex.css";
+import "primereact/resources/themes/lara-dark-amber/theme.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
