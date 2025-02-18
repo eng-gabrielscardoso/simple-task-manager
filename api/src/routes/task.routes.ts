72922f3f203677b095766be7fa82b7dd2e2a99e8
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2025-02-18 15:55:28 -0300

    Register show task method

--- api/src/routes/task.routes.ts
+++ api/src/routes/task.routes.ts
@@ -5,6 +5,7 @@ const router: Router = Router();
 
 router.get("/tasks", TaskController.index);
 router.post("/tasks", TaskController.store);
+router.get("/tasks/:id", TaskController.show);
 router.patch("/tasks/:id", TaskController.update);
 router.delete("/tasks/:id", TaskController.destroy);
 
