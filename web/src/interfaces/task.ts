b9b6e5a788e5b080bc5edea53c768d73cb3effbd
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2025-02-19 12:18:17 -0300

    Adjusts in schemas and interfaces

--- web/src/interfaces/task.ts
+++ web/src/interfaces/task.ts
@@ -3,3 +3,12 @@ export enum TaskStatus {
   IN_PROGRESS = "in_progress",
   COMPLETED = "completed",
 }
+
+export interface Task {
+  id: number
+  title: string
+  description?: string
+  status: TaskStatus
+  created_at: Date
+  updated_at: Date
+}
--- web/src/schemas/task.ts
+++ web/src/schemas/task.ts
@@ -8,6 +8,7 @@ export const createTaskSchema = z.object({
 })
 
 export const updateTaskSchema = z.object({
+  id: z.number(),
   title: z.string().min(3, 'The title must have at least 3 characters').max(120, 'The title must have at most 120 characters').optional(),
   description: z.string().min(3, 'The title must have at least 3 characters').max(120, 'The title must have at most 120 characters').optional(),
   status: z.nativeEnum(TaskStatus).optional(),
